b4aab04c6f4e0a5561152a8b56e09d3e65c53943
Richard Opiyo <romenyo@dewcis.com>
2022-02-02 19:36:29 +0300

    Implement dynamic JS for the features section

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('#show');
 const btnclose = document.querySelector('#hide');
 const showMoreBtn = document.querySelector('#showmore');
+const featuredSpeakersSect = document.querySelector('#dyna-bottom-section');
 
 // To open the menu bar
 function display() {
@@ -26,3 +27,58 @@ function toggle() {
 btn.addEventListener('click', display);
 btnclose.addEventListener('click', close);
 showMoreBtn.addEventListener('click', toggle);
+
+let featuredSpeakers = [
+  {
+    name : 'Yochai Benkler',
+    description : 'Havard Professor at Havard Technical College of Education',
+    notes : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia',
+    image :  './images/speakers/speaker-1.png'
+  },
+  {
+    name : 'Kamau Mwangi',
+    description : 'SVCET Professor at SVCET Technical College of Education',
+    notes : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia',
+    image :  './images/speakers/speaker-2.png'
+  },
+  {
+    name : 'Wachira Waruru',
+    description : 'VIT Professor at VIT Technical College of Education',
+    notes : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia',
+    image :  './images/speakers/speaker-3.png'
+  },
+  {
+    name : 'Robert Bonse',
+    description : 'TUK Professor at TUK Technical College of Education',
+    notes : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia',
+    image :  './images/speakers/speaker-4.png'
+  },
+  {
+    name : 'Kiplagat Kipchoge',
+    description : 'UON Professor at UON Technical College of Education',
+    notes : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia',
+    image :  './images/speakers/speaker-5.png'
+  },
+  {
+    name : 'Shreshta Nitu',
+    description : 'KU Professor at KU Technical College of Education',
+    notes : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia',
+    image :  './images/speakers/speaker-6.png'
+  }
+];
+
+featuredSpeakers.forEach((item) => {
+  const content = `<div class="col-md-6 top-feature">
+                      <div class="top-feature-1">
+                        <img src="${item.image}">
+                      </div>
+                      <div class="top-feature-2">
+                        <h4>${item.name}</h4>
+                        <h6><em>${item.description}</em></h6>
+                        <hr>
+                        <p>${item.notes}</p>
+                      </div>
+                    </div>`
+
+        featuredSpeakersSect.innerHTML += content ;
+});
